efc3db95d8932c467de721470f29181d866d0a00
educ8s <info@educ8s.tv>
2024-10-14 10:06:33 +0300

    1.01
    Code cleanup

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main () {
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
     int ball_x = 100;
     int ball_y = 100;
     int ball_speed_x = 5;
@@ -15,29 +15,29 @@ int main () {
 
     cout << "Hello World" << endl;
 
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false){
-        BeginDrawing();
-        ClearBackground(BLACK);
+   
         ball_x += ball_speed_x;
         ball_y += ball_speed_y;
 
-        if(ball_x + ball_radius >= screenWidth  || ball_x - ball_radius <= 0)
+        if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
         {
             ball_speed_x *= -1;
         }
 
-        if(ball_y + ball_radius >= screenHeight  || ball_y - ball_radius <= 0)
+        if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
         {
             ball_speed_y *= -1;
         }
-
-        DrawCircle(ball_x,ball_y,ball_radius, WHITE);
+        
+        BeginDrawing();
+            ClearBackground(BLACK);
+            DrawCircle(ball_x,ball_y,ball_radius, WHITE);
         EndDrawing();
     }
 
     CloseWindow();
-    return 0;
 }
